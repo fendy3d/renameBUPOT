44804b51b96695429faefd013abbb2a3357a3862
fendy3d <fendy3d@gmail.com>
2022-03-23 20:19:47 +0700

    3rd commit

--- renameBUPOT.py
+++ renameBUPOT.py
@@ -5,8 +5,8 @@ import csv
 
 pathToPdfs = os.getcwd()+"/dropPdfHere/"
 
-year = input("What year is this BUPOT? Eg: 2021, 2022 etc")
-month = input("What month is this BUPOT? Eg: 01,05,12 etc")
+# year = input("What year is this BUPOT? Eg: 2021, 2022 etc")
+# month = input("What month is this BUPOT? Eg: 01,05,12 etc")
 
 for _, _, files in os.walk(pathToPdfs):
     for filename in files:
@@ -19,6 +19,9 @@ for _, _, files in os.walk(pathToPdfs):
             list_of_texts = texts.split('\n')
             
             bupot = list_of_texts[6].split(':')[-1]
+            masa_pajak = list_of_texts[24].split(' ')[0]
+            month = "{:02d}".format(int(masa_pajak.split('-')[0])) # make the month has leading 0 if single digit
+            year = masa_pajak.split('-')[1]
 
             old_file_directory = pathToPdfs + filename
             new_name = year + '-' + month + '-' + bupot + '.pdf'
