0f5b54daafbf92c8c501c74fc71629ecfdac756b
fendy3d <fendy3d@gmail.com>
2022-09-13 18:15:48 +0700

    added entity code 2

--- renameBUPOT.py
+++ renameBUPOT.py
@@ -11,9 +11,11 @@ pathToPdfs = os.getcwd()+"/dropPdfHere/"
 def printText(list_of_texts):
     counter = 0
     for text in list_of_texts:
-        print(counter)
-        print(text)
+        print(str(counter) + ": " + text)
         counter += 1
+
+# Entity code: 1 (for old BUPOT), 2 (for new BUPOT)
+entity_code = 2
 
 for _, _, files in os.walk(pathToPdfs):
     for filename in files:
@@ -25,15 +27,24 @@ for _, _, files in os.walk(pathToPdfs):
             texts = page.extract_text()
             list_of_texts = texts.split('\n')
             
-            bupot = list_of_texts[6].split(':')[-1]
-            masa_pajak = list_of_texts[24].split(' ')[0]
-            month = "{:02d}".format(int(masa_pajak.split('-')[0])) # make the month has leading 0 if single digit
-            year = masa_pajak.split('-')[1]
-            entity = list_of_texts[33].split('Wajib Pajak : ')[-1]
-
             # printText(list_of_texts)
+            
+            if(entity_code == 1):
+                nomor_bupot = list_of_texts[6].split(':')[-1]
+                masa_pajak = list_of_texts[24].split(' ')[0]
+                month = "{:02d}".format(int(masa_pajak.split('-')[0])) # make the month has leading 0 if single digit
+                year = masa_pajak.split('-')[1]
+                entity = list_of_texts[33].split('Wajib Pajak : ')[-1]
+
+            elif(entity_code == 2):
+                nomor_bupot = list_of_texts[4].split(':')[-1].split(" H.4")[0].replace(" ", "")
+                masa_pajak = list_of_texts[15].split(' ')[0]
+                month, year = masa_pajak.split('-')
+                month = "{:02d}".format(int(month)) # make the month has leading 0 if single digit
+                entity = list_of_texts[10].split(' : ')[-1]
+            
             old_file_directory = pathToPdfs + filename
-            new_name = year + '-' + month + '-' + bupot + '-' + entity+'.pdf'
+            new_name = year + '-' + month + '-' + nomor_bupot + '-' + entity +'.pdf'
             new_file_directory = pathToPdfs + new_name
 
             pdf.close()
